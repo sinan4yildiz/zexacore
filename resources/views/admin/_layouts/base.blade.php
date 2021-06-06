49d5162a0b68f063e0058fbf83c725cf9e32a362
Sinan <snn@sinanyildiz.me>
2021-06-06 11:10:45 +0100

    Update base.blade.php

--- resources/views/admin/_layouts/base.blade.php
+++ resources/views/admin/_layouts/base.blade.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0 shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@lang('admin/common.brand_name')</title>
     <link href="https://fonts.googleapis.com/css2?family=Muli:wght@200;300;400;500;600&display=swap" rel="stylesheet">
