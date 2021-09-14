723d17817643194886e29ccbcf301d457e8d0c19
Sinan <snn@sinanyildiz.me>
2021-09-14 17:18:56 +0100

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,12 +2,24 @@ const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
 /*
+* Assets dirs
+*
+* */
+const assets = {
+  base: 'resources/assets/',
+
+  path: function (end) {
+    return this.base + end;
+  },
+};
+
+/*
  * Web
  *
  * */
 
 // Copy images directory
-mix.copyDirectory('resources/assets/web/images', 'public/assets/web/images');
+mix.copyDirectory(assets.path('web/images'), 'public/assets/web/images');
 
 /*
  * Admin
@@ -15,11 +27,11 @@ mix.copyDirectory('resources/assets/web/images', 'public/assets/web/images');
  * */
 
 // Scripts
-mix.js('resources/assets/admin/vue/application.js', 'assets/' + process.env.APP_ADMIN_DIR + '/js')
+mix.js(assets.path('admin/vue/application.js'), 'assets/' + process.env.APP_ADMIN_DIR + '/js')
   .version();
 
 // Styles
-mix.sass('resources/assets/admin/scss/app.scss', 'assets/' + process.env.APP_ADMIN_DIR + '/css')
+mix.sass(assets.path('admin/scss/app.scss'), 'assets/' + process.env.APP_ADMIN_DIR + '/css')
   .options({
     processCssUrls: false,
     postCss: [
@@ -29,18 +41,14 @@ mix.sass('resources/assets/admin/scss/app.scss', 'assets/' + process.env.APP_ADM
   .version();
 
 // Copy images directory
-mix.copyDirectory('resources/assets/admin/images', 'public/assets/' + process.env.APP_ADMIN_DIR + '/images');
+mix.copyDirectory(assets.path('admin/images'), 'public/assets/' + process.env.APP_ADMIN_DIR + '/images');
 
-// ESLint Loader
-/*mix.webpackConfig({
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.(js|vue)$/,
-                loader: 'eslint-loader',
-                exclude: /node_modules/
-            }
-        ]
-    }
-})*/
+// Webpack config
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, assets.path('admin/scss')),
+      '@': path.resolve(__dirname, assets.path('admin/vue')),
+    },
+  },
+});
