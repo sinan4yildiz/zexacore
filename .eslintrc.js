2d6282950911cb5419fa1da77d3be474bf7b3e60
Sinan <snn@sinanyildiz.me>
2021-09-14 17:19:30 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     es2021: true,
     browser: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
@@ -15,11 +16,23 @@ module.exports = {
   },
   plugins: [
     'vue',
+    'jest',
     'tailwindcss',
   ],
   globals: {
     '_': 'readonly',
     'axios': 'readonly',
+    'appConfig': 'readonly',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './resources/assets/admin/vue'],
+        ],
+        extensions: ['.vue', '.js'],
+      },
+    },
   },
   rules: {
     'func-names': ['error', 'never'],
@@ -27,5 +40,16 @@ module.exports = {
     'no-underscore-dangle': ['error', { allow: ['_uid'] }],
     'no-multi-assign': ['error', { ignoreNonDeclaration: true }],
     'max-len': [1, { code: 300, ignorePattern: '<\\s*path|symbol[^>]*>(.*?)' }],
+
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never',
+    }],
   },
 };
