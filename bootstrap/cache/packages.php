983d8bf2e67f207caebe334acde1a8c02734ca0e
Sinan Yildiz <snn@sinanyildiz.me>
2021-10-24 13:14:56 +0100

    Usual commit

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -42,13 +42,6 @@
       0 => 'Laravel\\Tinker\\TinkerServiceProvider',
     ),
   ),
-  'laravel/ui' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Ui\\UiServiceProvider',
-    ),
-  ),
   'nesbot/carbon' => 
   array (
     'providers' => 
